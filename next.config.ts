81dcd6d6dc40811c66d85c65908b4560dfc8be37
daffaalex22 <daffaalex22@gmail.com>
2025-02-24 21:33:44 +0700

    added cardlist and navbar

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,24 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatar.iran.liara.run',
+        port: '',
+        pathname: '/**/**',
+        search: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.aceternity.com',
+        port: '',
+        pathname: '/**/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
